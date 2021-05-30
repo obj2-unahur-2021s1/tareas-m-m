4275795e4519ecfb7d87b5cde565f7d898c5a452
unknown <tomasmiranda98@hotmail.com>
2021-05-30 20:58:44 -0300

    se agrega funcionalidad para agregar a los trabajadores y diferenciar de obrero de supervisor

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -3,14 +3,31 @@ package ar.edu.unahur.obj2.tareas
 class Tarea(var horasRequeridas: Double){
     val trabajadoresEnLaTarea = mutableListOf<Trabajador>()
 
+    var yaSeAsignoAUnResponsabe = false
+
     fun nominaDeEmpleados() = trabajadoresEnLaTarea.size
 
-    fun agregarUnTrabajador(trabajadorAAgregar: Trabajador) = trabajadoresEnLaTarea.add(trabajadorAAgregar)
+    fun agregarUnTrabajador(trabajadorAAgregar: Trabajador){
+        if(trabajadorAAgregar.sirveParaHacerLaTarea()){
+            trabajadoresEnLaTarea.add(trabajadorAAgregar)
+        }
+        else if(!trabajadorAAgregar.sirveParaHacerLaTarea() and !yaSeAsignoAUnResponsabe){
+            trabajadoresEnLaTarea.add(trabajadorAAgregar)
+            yaSeAsignoAUnResponsabe = true
+        }
+
+    }
 }
 
+
+
 //pensando seriamente en que deberia ser inteface
-open class Trabajador(var cuantoCobraPorHora: Int){}
-
-class Empleado(cuantoCobraPorHora: Int): Trabajador(cuantoCobraPorHora) {}
-
-class Responsable(cuantoCobraPorHora: Int): Trabajador(cuantoCobraPorHora){}
+abstract class Trabajador(var cuantoCobraPorHora: Int){
+    abstract fun sirveParaHacerLaTarea(): Boolean
+}
+class Empleado(cuantoCobraPorHora: Int): Trabajador(cuantoCobraPorHora) {
+    override fun sirveParaHacerLaTarea() = true
+}
+class Responsable(cuantoCobraPorHora: Int): Trabajador(cuantoCobraPorHora){
+    override fun sirveParaHacerLaTarea() = false
+}
