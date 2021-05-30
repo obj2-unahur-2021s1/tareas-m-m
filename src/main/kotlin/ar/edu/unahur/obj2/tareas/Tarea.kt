8d7de2168df1dc964b3a3b3031a17101c20e541e
unknown <tomasmiranda98@hotmail.com>
2021-05-30 19:28:30 -0300

    se agrega idea base resolver el enunciado

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,3 +1,16 @@
 package ar.edu.unahur.obj2.tareas
 
-class Tarea
+class Tarea(var horasRequeridas: Double){
+    val trabajadoresEnLaTarea = mutableListOf<Trabajador>()
+
+    fun nominaDeEmpleados() = trabajadoresEnLaTarea.size
+
+    fun agregarUnTrabajador(trabajadorAAgregar: Trabajador) = trabajadoresEnLaTarea.add(trabajadorAAgregar)
+}
+
+//pensando seriamente en que deberia ser inteface
+open class Trabajador(var cuantoCobraPorHora: Int){}
+
+class Empleado(cuantoCobraPorHora: Int): Trabajador(cuantoCobraPorHora) {}
+
+class Responsable(cuantoCobraPorHora: Int): Trabajador(cuantoCobraPorHora){}
