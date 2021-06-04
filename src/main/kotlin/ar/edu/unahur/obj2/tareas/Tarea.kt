a3ec38111c4124968e1712d52755e0353578190d
marcarand <marcarand@riseup.net>
2021-06-04 02:38:58 -0300

    rama alternativa

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,10 +1,15 @@
 package ar.edu.unahur.obj2.tareas
 
-class Tarea(var horasEstimadas: Double){
-    val empleadosAsignados = mutableListOf<Trabajador>()
-    var responsable : Responsable ? = null
+class Tarea(val horasEstimadas: Double, val costoInfraestructura: Double) {
 
-    var yaSeAsignoAUnResponsabe = responsable != null
+    val empleadosAsignados = mutableListOf<Trabajador>()    // ver si se puede pasar por parámetro
+    // var yaSeAsignoAUnResponsabe = responsable != null
+
+    // esta función debería ser el promedio (si entendí bien) de los sueldos de todos los empleados
+    // si es así, entonces habría que recorrer la lista de empleados, sumar sus sueldos y dividirlos
+    // por la cantidad total de empleados.
+    fun sueldoPorHora() = 400.99 // pongo este valor de manera temporal para que funcione costoTarea
+                               // pero hay que modificar la función.
 
     // fun nominaDeEmpleados() = empleadosAsignados.size
     // me parece que consultar sería mostrar la lista de empleados y mostrar
@@ -14,12 +19,16 @@ class Tarea(var horasEstimadas: Double){
         empleadosAsignados.forEach {
             println(it)
         }
+        /*
         when(yaSeAsignoAUnResponsabe) {
             true -> println("Responsable de la tarea: " + responsable)
             else -> println("No hay un responsable asignado aún")
         }
+        */
     }
 
+    // ver si se puede pasar directamente la lista por parámetro al objeto
+    /*
     fun asignarTrabajador(trabajadorAAgregar: Trabajador){
         if(trabajadorAAgregar.sirveParaHacerLaTarea()){
             empleadosAsignados.add(trabajadorAAgregar)
@@ -30,25 +39,34 @@ class Tarea(var horasEstimadas: Double){
         }
 
     }
+    */
 
-    fun asignarResponsable(responsableDeTarea: Responsable) {
-        responsable = responsableDeTarea
+    fun asignarTrabajador(nuevoEmpleado: Trabajador) {
+        empleadosAsignados.add(nuevoEmpleado)
     }
 
-    fun cantidadDeEmpleados() = empleadosAsignados.size
+    fun cantidadDeEmpleados() = empleadosAsignados.size -1
 
     fun horasNecesarias() = horasEstimadas / this.cantidadDeEmpleados()
+
+    fun costoTarea() = horasNecesarias() * sueldoPorHora() + horasEstimadas * responsable.cuantoCobraPorHora + costoInfraestructura
 }
 
 
+// No entiendo por qué le pusiste la función sirveParaHacerLaTarea()... no está en la consigna y
+// complica el código.
+// ahí propongo alternativa
 
 //pensando seriamente en que deberia ser inteface
 abstract class Trabajador(var cuantoCobraPorHora: Int){
-    abstract fun sirveParaHacerLaTarea(): Boolean
+    // abstract fun sirveParaHacerLaTarea(): Boolean
+    abstract fun esResponsable() : Boolean
 }
 class Empleado(cuantoCobraPorHora: Int): Trabajador(cuantoCobraPorHora) {
-    override fun sirveParaHacerLaTarea() = true
+    //     override fun sirveParaHacerLaTarea() = true
+    override fun esResponsable() = false
 }
 class Responsable(cuantoCobraPorHora: Int): Trabajador(cuantoCobraPorHora){
-    override fun sirveParaHacerLaTarea() = false
+    // override fun sirveParaHacerLaTarea() = false
+    override fun esResponsable() = true
 }
--- src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
@@ -6,13 +6,15 @@ import io.kotest.matchers.shouldBe
 
 class TareaTest : DescribeSpec({
   describe("Tarea") {
-    var tarea1 = Tarea(8.5)
+
     val empleado1 = Empleado(400)
     val empleado2 = Empleado(400)
     val responsable1 = Responsable(600)
 
-    tarea1.asignarTrabajador(empleado1)
-    tarea1.asignarTrabajador(empleado2)
+    var tarea1 = Tarea(responsable1,8.5, 25999.99)
+
+    //tarea1.asignarTrabajador(empleado1)
+    //tarea1.asignarTrabajador(empleado2)
 
     it("Consultar nómina de una tarea") {
       tarea1.nominaDeEmpleados().shouldBe(kotlin.Unit)    // revisar esto, porque es una salida de
@@ -21,12 +23,14 @@ class TareaTest : DescribeSpec({
                                                           // es la consulta.
     }
 
+    /*
     it("Consultar nómina con un responsable") {
       tarea1.asignarResponsable(responsable1)
       tarea1.nominaDeEmpleados().shouldBe(kotlin.Unit)    // idem - después de saber cómo es la
                                                           // consulta, se pueden aprovechar estos
                                                           // dos test distintos.
     }
+    */
 
     it("4.25 horas necesarias para finalizar la tarea") {
       tarea1.horasNecesarias().shouldBe(4.25)
