9d8f2373837bf053cefba8ccefd44432081e04ee
marcarand <marcarand@riseup.net>
2021-06-04 01:41:23 -0300

    test cantidad hs para finalizar tarea

--- src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
@@ -11,9 +11,10 @@ class TareaTest : DescribeSpec({
     val empleado2 = Empleado(400)
     val responsable1 = Responsable(600)
 
+    tarea1.asignarTrabajador(empleado1)
+    tarea1.asignarTrabajador(empleado2)
+
     it("Consultar nómina de una tarea") {
-      tarea1.asignarTrabajador(empleado1)
-      tarea1.asignarTrabajador(empleado2)
       tarea1.nominaDeEmpleados().shouldBe(kotlin.Unit)    // revisar esto, porque es una salida de
                                                           // tipo String y no permite corroborar la
                                                           // lista. El enunciado no especifica cómo
@@ -21,11 +22,14 @@ class TareaTest : DescribeSpec({
     }
 
     it("Consultar nómina con un responsable") {
-      tarea1.asignarTrabajador(empleado1)
       tarea1.asignarResponsable(responsable1)
       tarea1.nominaDeEmpleados().shouldBe(kotlin.Unit)    // idem - después de saber cómo es la
                                                           // consulta, se pueden aprovechar estos
                                                           // dos test distintos.
+    }
+
+    it("4.25 horas necesarias para finalizar la tarea") {
+      tarea1.horasNecesarias().shouldBe(4.25)
     }
   }
 })
